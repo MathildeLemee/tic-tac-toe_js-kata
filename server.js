ca5025a73f7e55579e88a35f4a61f26e4ee4c7c0
Mathilde Lemee <mathilde.lemee@yahoo.fr>
2011-09-18 18:07:19 +0200

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/static/index.html');
 });
-app.listen();
+app.listen(process.env.PORT||9090, "0.0.0.0");
 
 
 
