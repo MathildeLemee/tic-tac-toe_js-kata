1d974342f4a6df5a78e6b3ffacd17d3d06759503
Mathilde Lemee <mathilde.lemee@yahoo.fr>
2011-09-20 23:48:34 +0200

    small refactoring with express

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var redis = require("redis"),
        console.log(err);
     });
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/static'));
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/static/index.html');
 });
